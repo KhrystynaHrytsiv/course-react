1b01618d9b4536d8b649091b84bc8cd430b0cc71
KhrystynaHrytsiv <khrystyna.hrytsiv@gmail.com>
2026-03-31 14:15:53 +0300

    hw 1 task 1

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,18 @@ import './App.css'
 
 
 function App() {
-
+  const coursesTitleArray:string []= [
+    'JavaScript Complex',
+    'Java Complex',
+    'Python Complex',
+    'QA Complex',
+    'Fullstack',
+    'Frontend'
+  ];
   return (
-    <>
-
-    </>
+    <ul>
+      {coursesTitleArray.map((course, index) =><li key={index}>{course}</li>)}
+    </ul>
   )
 }
 
