ecbff02f1b63f80de0b34ad01ed570d6556c136b
KhrystynaHrytsiv <khrystyna.hrytsiv@gmail.com>
2026-04-01 15:29:00 +0300

    hw 2 task 4 updated

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -9,11 +9,37 @@ const Product:FC<IProp> = ({product}) => {
     return (
         <div className={'block'}>
             <h3>{product.title}</h3>
-            <div>Price: {product.price}</div>
-            <div>Brand: {product.brand}</div>
-            <div>Category: {product.category}</div>
             <div>{product.description}</div>
+            <div>Category: {product.category}</div>
+            <div>Price: {product.price}</div>
+            <div>Discount Percentage: {product.discountPercentage}</div>
+            <div>Rating: {product.rating}</div>
+            <div>Stock: {product.stock}</div>
+            <div>Tags: {product.tags}</div>
+            <div>Brand: {product.brand}</div>
+            <div>Sku: {product.sku}</div>
+            <div>Weight: {product.weight}</div>
+            <div>Dimensions height: {product.dimensions.height}</div>
+            <div>Warranty Information: {product.warrantyInformation}</div>
+            <div>Shipping Information: {product.shippingInformation}</div>
+            <div>Availability Status: {product.availabilityStatus}</div>
+                <div className={'reviewsContainer'}>Reviews:
+                        {product.reviews.map(review =>
+                            <div>
+                                <div>Rating: {review.rating}</div>
+                                <div>{review.comment}</div>
+                                <div>Date: {review.date}</div>
+                                <div>ReviewerName: {review.reviewerName}</div>
+                                <div>ReviewerEmail: {review.reviewerEmail}</div>
+                            </div>)}
+                </div>
+            <div>Return Policy: {product.returnPolicy}</div>
+            <div>Minimum Order Quantity: {product.minimumOrderQuantity}</div>
+            <div>Created at: {product.meta.createdAt}</div>
             <img src={product.thumbnail} alt={product.title}/>
+                {product.images.length > 1 &&( <div className={'imageContainer'}>images:
+                    {product.images.map(img => <img src={img} alt={'photo'}/>)}
+            </div>)}
         </div>
     );
 };
