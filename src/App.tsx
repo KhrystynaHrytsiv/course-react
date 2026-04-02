3869f5343f793a68d406f7bb9bf4d2e7d4f46660
KhrystynaHrytsiv <khrystyna.hrytsiv@gmail.com>
2026-04-02 19:06:27 +0300

    lesson 5 controlled form

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ function App() {
   return (
     <div>
       <Form/>
-      {/*<div contentEditable={true}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-       A accusamus aspernatur assumenda atque, consectetur cum dolor doloribus iste natus neque, nostrum odio quasi quod ratione rem repudiandae velit voluptate! Consequatur corporis dicta dignissimos error esse incidunt maxime minus, molestiae molestias nesciunt nihil nostrum odio porro, quod ut. Vero, voluptatibus voluptatum.</div>*/}
     </div>
   )
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,55 @@
+import {type SyntheticEvent, useState} from "react";
+
+interface IForm{
+    username:string,
+    password:string
+}
 const Form = () => {
+    const [formState, setFormState] = useState<IForm>({username:'hello', password: '1111'});
+
+
+    const handleSubmit = (ev:SyntheticEvent<HTMLFormElement>) => {
+        ev.preventDefault();
+        // console.log(ev);
+        // console.log(ev.target);
+        // const form = ev.target as HTMLFormElement;
+        // console.log(form);
+        // console.log(form.username.value);
+        // console.log(form.password.value);
+        const user ={
+            username: formState.username,
+            password:formState.password
+        };
+        console.log(user);
+    }
+
+
+    // const handleUsernameChange = (e:FormEvent<HTMLInputElement>) => {
+    //     const input = e.target as HTMLInputElement;
+    //     console.log(input.value);
+    //     setFormState({...formState, username:input.value})
+    // }
+    // const handlePasswordChange = (e:FormEvent<HTMLInputElement>) => {
+    //     const input = e.target as HTMLInputElement;
+    //     console.log(input.value);
+    //     setFormState({...formState, password:input.value})
+    // }
+
+    const handleInputChange = (e:SyntheticEvent<HTMLInputElement>) => {
+        const input = e.target as HTMLInputElement;
+        console.log(input.name);
+        setFormState({...formState, [input.name]: input.value})
+    };
+
     return (
         <div>
-            <form>
-                <input type="text" name={'username'}/>
-                <input type="text" name={'password'}/>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name={'username'} value={formState.username} onChange={handleInputChange}/>
+                <input type="text" name={'password'} value={formState.password} onChange={handleInputChange}/>
+                <button>send</button>
             </form>
         </div>
-    );
+    )
 };
 
 export {Form};
