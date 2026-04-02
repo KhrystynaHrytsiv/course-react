f26fe3d1317ecf381f1414efe405e01e81642eb9
KhrystynaHrytsiv <khrystyna.hrytsiv@gmail.com>
2026-04-02 13:48:27 +0300

    hw 3 task 6

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,10 +1,11 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, Navigate} from "react-router-dom";
 import {MainLayout} from "../layout/MainLayout.tsx";
 import {UsersPage} from "../pages/UsersPage.tsx";
 import {UserCartsPage} from "../pages/UserCartsPage.tsx";
 
 export const router = createBrowserRouter([
     {path: '', element:<MainLayout/>, children:[
+            {index:true, element:<Navigate to={'users'}/>},
             {path:'users', element:<UsersPage/>},
             {path:'users/:userId/carts', element:<UserCartsPage/>}
 
