55d08993034d0f071573456672c9e8cc273cc8af
KhrystynaHrytsiv <khrystyna.hrytsiv@gmail.com>
2026-04-02 19:40:21 +0300

    lesson 5 validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,17 +6,36 @@ interface IForm{
     age:number
 }
 const Form = () => {
-    const {handleSubmit, register} = useForm<IForm>();
+    const {handleSubmit, register, formState:{errors, isValid}} = useForm<IForm>({mode:'all'});
     const handler =(formData:IForm) =>{
         console.log(formData)
     }
     return (
         <div>
             <form onSubmit={handleSubmit(handler) }>
-                <input type="text" {...register('username')} />
-                <input type="text"  {...register('password')} />
-                <input type="number"  {...register('age')} />
-                <button>send</button>
+                <label><input type="text" {...register('username',
+                    {required:true,
+                        // pattern:{value:/\w+/, message: 'wrong username'},
+                        minLength:{value:1, message: 'incorrect name'}
+                    })} />
+                    {errors.username && <div>{errors.username.message}</div>}
+                </label>
+                <label><input type="text"  {...register('password', {
+                    required: true,
+                    minLength: {value: 3, message: 'pass is too short'},
+                    maxLength: {value: 7, message: 'pass is too long'}
+                })} />
+                    {errors.password && <div>{errors.password.message}</div>}
+                </label>
+                <label><input type="number"  {...register('age', {
+                    required: true,
+                    valueAsNumber: true,
+                    min: {value: 1, message: 'age too small'},
+                    max: {value: 117, message: 'age too big'},
+                })} />
+                    {errors.age && <div>{errors.age.message}</div>}
+                </label>
+                <button disabled={!isValid}>send</button>
             </form>
         </div>
     )
