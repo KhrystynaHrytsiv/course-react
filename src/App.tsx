7f11593d77e686f578ff033727d1372857f0db04
KhrystynaHrytsiv <khrystyna.hrytsiv@gmail.com>
2026-04-02 18:11:44 +0300

    lesson 5 uncontrolled form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
+import {Form} from "./components/Form.tsx";
 
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <div>
+      <Form/>
+      {/*<div contentEditable={true}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+       A accusamus aspernatur assumenda atque, consectetur cum dolor doloribus iste natus neque, nostrum odio quasi quod ratione rem repudiandae velit voluptate! Consequatur corporis dicta dignissimos error esse incidunt maxime minus, molestiae molestias nesciunt nihil nostrum odio porro, quod ut. Vero, voluptatibus voluptatum.</div>*/}
+    </div>
   )
 }
 
---
+++ src/components/Form.tsx
@@ -0,0 +1,12 @@
+const Form = () => {
+    return (
+        <div>
+            <form>
+                <input type="text" name={'username'}/>
+                <input type="text" name={'password'}/>
+            </form>
+        </div>
+    );
+};
+
+export {Form};
