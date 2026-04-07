3b959b17e6e067b38933705035fa3156a674b4e4
KhrystynaHrytsiv <khrystyna.hrytsiv@gmail.com>
2026-04-07 12:37:48 +0300

    hw 7 task 1

--- src/components/Child.tsx
+++ src/components/Child.tsx
@@ -1,6 +1,6 @@
 import {type FC, memo, useMemo} from "react";
 
-const Child:FC<{callBack:() => void, count:number}> = memo(({ count}) => {
+const Child:FC<{callBack:() => void, count:number}> = memo(({count}) => {
     //запам'ятовує компонент і не дає йому переренджувтись, якщо пропси не змінились
     const memory = useMemo(() => {
         for (let i = 0; i < 10; i++) {
@@ -8,6 +8,7 @@ const Child:FC<{callBack:() => void, count:number}> = memo(({ count}) => {
         }
         return null
     }, []);//запам'ятовує результат обчислень і рендеритьься лише у випадку зміни результату
+
     return (
         <div>
             <p>Count: {count}</p>
